0109e909056cf05600d08b958e9a237870c83c37
geraldik <geraldik1986@gmail.com>
2022-08-19 23:24:15 +0300

    3. HQL [#6874 #334313]

--- src/main/java/ru/job4j/hql/HbmRun.java
+++ src/main/java/ru/job4j/hql/HbmRun.java
@@ -2,6 +2,7 @@ package ru.job4j.hql;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -16,34 +17,30 @@ public class HbmRun {
                 .configure().build();
         try {
             SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-            Session session = sf.openSession();
-            session.beginTransaction();
 
             Candidate one = Candidate.of("Ivan", 5, 500);
             Candidate two = Candidate.of("Oleg", 7, 900);
             Candidate three = Candidate.of("Olga", 2, 300);
 
-            session.save(one);
-            session.save(two);
-            session.save(three);
+            create(one, sf);
+            create(two, sf);
+            create(three, sf);
 
-            System.out.println(findById(1, session));
+            System.out.println(findById(1, sf));
 
-            for (Object st : findAll(session)) {
+            for (Object st : findAll(sf)) {
                 System.out.println(st);
             }
 
-            for (Object st : findByName("Ivan", session)) {
+            for (Object st : findByName("Ivan", sf)) {
                 System.out.println(st);
             }
 
-            update(1, "Ivan ", 6, 600, session);
-            System.out.println(findById(1, session));
+            update(1, "Ivan ", 6, 600, sf);
+            System.out.println(findById(1, sf));
 
-            delete(1, session);
+            delete(1, sf);
 
-            session.getTransaction().commit();
-            session.close();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -51,24 +48,48 @@ public class HbmRun {
         }
     }
 
-    public static List<Candidate> findAll(Session session) {
-        Query query = session.createQuery("from Candidate");
-        return query.list();
+    public static void create(Candidate candidate, SessionFactory sf) {
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(candidate);
+        transaction.commit();
+        session.close();
     }
 
-    public static Candidate findById(int id, Session session) {
-        Query<Candidate> query = session.createQuery("from Candidate c where c.id = :fId");
-        query.setParameter("fId", id);
-        return query.uniqueResult();
+    public static List<Candidate> findAll(SessionFactory sf) {
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Candidate> candidates = session.createQuery("from Candidate").list();
+        transaction.commit();
+        session.close();
+        return candidates;
     }
 
-    public static List<Candidate> findByName(String name, Session session) {
-        Query<Candidate> query = session.createQuery("from Candidate c where c.name = :fName")
-                .setParameter("fName", name);
-        return query.list();
+    public static Candidate findById(int id, SessionFactory sf) {
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        Candidate candidate = (Candidate) session.createQuery("from Candidate c where c.id = :fId")
+                                        .setParameter("fId", id)
+                                        .uniqueResult();
+        transaction.commit();
+        session.close();
+        return candidate;
     }
 
-    public static void update(int id, String newName, int newExperience, int newSalary, Session session) {
+    public static List<Candidate> findByName(String name, SessionFactory sf) {
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Candidate> candidates = session.createQuery("from Candidate c where c.name = :fName")
+                .setParameter("fName", name)
+                .list();
+        transaction.commit();
+        session.close();
+        return candidates;
+    }
+
+    public static void update(int id, String newName, int newExperience, int newSalary, SessionFactory sf) {
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
         session.createQuery("update Candidate c set c.name = :newName, "
                         + "c.experience = :newExperience, c.salary = :newSalary where c.id = :fId")
                 .setParameter("newName", newName)
@@ -76,11 +97,17 @@ public class HbmRun {
                 .setParameter("newSalary", newSalary)
                 .setParameter("fId", id)
                 .executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
-    public static void delete(int id, Session session) {
+    public static void delete(int id, SessionFactory sf) {
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
         session.createQuery("delete from Candidate where id = :fId")
                 .setParameter("fId", id)
                 .executeUpdate();
+        transaction.commit();
+        session.close();
     }
 }
